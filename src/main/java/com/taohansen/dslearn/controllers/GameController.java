a6bfde3f6478f5aeb4d53c4825bb5fc1c3632c01
Tao Hansen <taohansens@gmail.com>
2023-11-15 16:17:03 -0300

    Game By ID

--- src/main/java/com/taohansen/dslearn/controllers/GameController.java
+++ src/main/java/com/taohansen/dslearn/controllers/GameController.java
@@ -1,10 +1,13 @@
 package com.taohansen.dslearn.controllers;
 
+import com.taohansen.dslearn.dto.GameDTO;
 import com.taohansen.dslearn.dto.GameMinDTO;
 import com.taohansen.dslearn.entities.Game;
 import com.taohansen.dslearn.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +21,14 @@ public class GameController {
 	private GameService gameService;
 
 	@GetMapping
-	public List<GameMinDTO> findAll() {
+	public ResponseEntity<List<GameMinDTO>> findAll() {
 		List<GameMinDTO> result = gameService.findAll();
-		return result;
+		return ResponseEntity.ok().body(result);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<GameDTO> findById(@PathVariable Long id) {
+		GameDTO result = gameService.findById(id);
+		return ResponseEntity.ok().body(result);
 	}
 }
--- src/main/java/com/taohansen/dslearn/services/GameService.java
+++ src/main/java/com/taohansen/dslearn/services/GameService.java
@@ -1,12 +1,16 @@
 package com.taohansen.dslearn.services;
 
+import com.taohansen.dslearn.dto.GameDTO;
 import com.taohansen.dslearn.dto.GameMinDTO;
 import com.taohansen.dslearn.entities.Game;
 import com.taohansen.dslearn.repositories.GameRepository;
+import com.taohansen.dslearn.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GameService {
@@ -14,8 +18,16 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(GameMinDTO::new).toList();
+	}
+
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Optional<Game> result = gameRepository.findById(id);
+		Game entity = result.orElseThrow(() -> new ResourceNotFoundException("Entity Game not found."));
+		return new GameDTO(entity);
 	}
 }
