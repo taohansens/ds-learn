507c9b8be62acaf0bd41b5a3c8ad1fe6533a89ec
Tao Hansen <taohansens@gmail.com>
2023-11-16 21:22:46 -0300

    Homolog ok

--- src/main/java/com/taohansen/dslearn/projections/GameMinProjection.java
+++ src/main/java/com/taohansen/dslearn/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.taohansen.dslearn.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
